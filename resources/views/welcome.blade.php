7f0a58b60e1b3adf4130065e6c1ab99444c4f346
Adaptaweb <a.tamayo.web@gmail.com>
2017-10-31 17:27:02 -0300

    no se carga la otra wea xd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Mario QLO</title>
+        <title>Mario QLOsss</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
